70437d4e540a8988670d935a2773219f3064b8ad
kkm0406 <sdjmc30412@naver.com>
2023-03-26 20:41:32 +0900

    스프링부트 Delete Method

--- demo/src/main/java/com/example/demo/controller/DeleteController.java
+++ demo/src/main/java/com/example/demo/controller/DeleteController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/delete-api")
 public class DeleteController {
     //http://localhost:8080/api/v1/delete-api/variable1/{String 값}
-    //@DeleteMapping(value = "/delete/{variable}")
+    @DeleteMapping(value = "/delete/{variable}")
     public String deleteVariable(@PathVariable String variable){
         return variable;
     }
